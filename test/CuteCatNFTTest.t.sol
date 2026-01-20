d5359c48fc296a74816ff069b7991bf1bf907da5
PixelSyntax <amitthapa181133@gmail.com>
2026-01-20 23:59:35 +0530

    Add contract declaration for CuteCatNFTTest

--- test/CuteCatNFTTest.t.sol
+++ test/CuteCatNFTTest.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 import "forge-std/Test.sol";
 import "../src/CuteCatNFT.sol";
 
+
 contract CuteCatNFTTest is Test {
     CuteCatNFT nft;
     address owner = address(0xA1);
