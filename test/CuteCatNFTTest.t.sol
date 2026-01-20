5ec7d5c684351419c68a4431594fcf90a426eaa9
PixelSyntax <amitthapa181133@gmail.com>
2026-01-21 00:00:05 +0530

    Remove commented lines in CuteCatNFTTest

--- test/CuteCatNFTTest.t.sol
+++ test/CuteCatNFTTest.t.sol
@@ -22,11 +22,11 @@ contract CuteCatNFTTest is Test {
         vm.prank(owner);
         nft = new CuteCatNFT(baseURI);
     }
-    //Owner 
+
     function testOwner() public {
         assertEq(nft.owner(), owner);
     }
-    //Active
+
     function testMintActiveInitially() public {
         assertTrue(nft.mintActive());
     }
